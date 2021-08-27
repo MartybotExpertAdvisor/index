80576d2fd92056f86aef8a5991a8fdfc039f2ff4
SilentEATrader <r.guzman.online@gmail.com>
2021-08-27 17:10:08 +0800

    add tawk.to on client portal

--- zzz-js/client-portal.js
+++ zzz-js/client-portal.js
@@ -64,3 +64,16 @@ event = (event || window.event);
 }
 
 //^^^^^^   DISABLE RIGHT CLICK, CTRL, F12 to explore the contents of WEB PAGE   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+//^^^^^^Start of Tawk.to Script ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/612890ba649e0a0a5cd33124/1fe37an4q';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
+
+//^^^^^^End of Tawk.to Script ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
