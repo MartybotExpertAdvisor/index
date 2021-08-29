257269e050f5e11ce7b5f0ce20d076ab96f2e069
SilentEATrader <r.guzman.online@gmail.com>
2021-08-29 16:16:20 +0800

    fixed superman iron animation

--- zzz-js/client-portal.js
+++ zzz-js/client-portal.js
@@ -6,6 +6,20 @@ function vanish() {
 }
 //^^^^^^   PRE LOADER to COMPLETE WEB PAGE contents    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
+//vvvvvvv   Superman Dispaly off    vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv 
+const animation = document.querySelector('.animation');
+window.onload = function()
+                        {
+                            setTimeout
+                            (
+                                function()
+                                {
+                                    animation.style.display = "block"
+                                    //add some time delay to show popup
+                                }, 5000)
+                        }
+//^^^^^^   ENDS Superman Dispaly off    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
 // vvvvvvv  DISABLE RIGHT CLICK, CTRL, F12 to explore the contents of WEB PAGE  vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv 
 // Disable the rightclick and ctrl and f12 keys to find the source file
 // take body to change the content
@@ -14,40 +28,40 @@ const body = document.getElementsByTagName('body');
 window.addEventListener("keydown", (event) => {
     if(event.ctrlKey && (event.key === "S" || event.key === "s")) {
         event.preventDefault();
-        alert("Sorry, you can't do this 💔");
+        //alert("Sorry, you can't do this 💔");
     }
 
     if(event.ctrlKey && (event.key === "C")) {
         event.preventDefault();
-        alert("Sorry, you can't do this 💔");
+        //alert("Sorry, you can't do this 💔");
     }
     if(event.ctrlKey && (event.key === "E" || event.key === "e")) {
         event.preventDefault();
-        alert("Sorry, you can't do this 💔");
+        //alert("Sorry, you can't do this 💔");
     }
     if(event.ctrlKey && (event.key === "I" || event.key === "i")) {
         event.preventDefault();
-        alert("Sorry, you can't do this 💔");
+        //alert("Sorry, you can't do this 💔");
     }
     if(event.ctrlKey && (event.key === "K" || event.key === "k")) {
         event.preventDefault();
-        alert("Sorry, you can't do this 💔");
+        //alert("Sorry, you can't do this 💔");
     }
     if(event.ctrlKey && (event.key === "U" || event.key === "u")) {
         event.preventDefault();
-        alert("Sorry, you can't do this 💔");
+        //alert("Sorry, you can't do this 💔");
     }
 });
 
 if (document.addEventListener) {
     document.addEventListener('contextmenu', function(e) {
-    alert("Sorry, you can't do this 💔");
+    //alert("Sorry, you can't do this 💔");
     e.preventDefault();
     }, false);
 }
 else {
     document.attachEvent('oncontextmenu', function() {
-    alert("Sorry, you can't do this 💔");
+    //alert("Sorry, you can't do this 💔");
     window.event.returnValue = false;
     });
 }
@@ -58,7 +72,7 @@ document.onkeydown = function (event)
 event = (event || window.event);
     if (event.keyCode == 123 || event.keyCode == 18)
     {
-        alert("Sorry, you can't do this 💔");
+        //alert("Sorry, you can't do this 💔");
         return false;
     }
 }
